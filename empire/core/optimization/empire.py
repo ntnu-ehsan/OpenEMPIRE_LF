f514f63e6dd0b34ed8314dc3c2fb0df6c3f1e12c
WouterKoksNL <wouterkoks135@live.nl>
2025-09-03 13:26:58 +0200

    separate set_solver

--- empire/core/optimization/empire.py
+++ empire/core/optimization/empire.py
@@ -777,24 +777,7 @@ def run_empire(name,
 
     logger.info("Solving...")
 
-    if solver == "CPLEX":
-        opt = SolverFactory("cplex", Verbose=True)
-        opt.options["lpmethod"] = 4
-        opt.options["solutiontype"] = 2
-        #instance.display('outputs_cplex.txt')
-    if solver == "Xpress":
-        opt = SolverFactory("xpress") #Verbose=True
-        opt.options["defaultAlg"] = 4
-        opt.options["crossover"] = 0
-        opt.options["lpLog"] = 1
-        opt.options["Trace"] = 1
-        #instance.display('outputs_xpress.txt')
-    if solver == "Gurobi":
-        opt = SolverFactory('gurobi', Verbose=True)
-        opt.options["Crossover"]=0
-        opt.options["Method"]=2
-    if solver == "GLPK":
-        opt = SolverFactory("glpk", Verbose=True)
+    opt = set_solver(solver)
 
     opt.solve(instance, tee=True, logfile=result_file_path / f"logfile_{name}.log")#, keepfiles=True, symbolic_solver_labels=True)
 
@@ -817,3 +800,38 @@ def run_empire(name,
     if OPERATIONAL_DUALS and not OUT_OF_SAMPLE:
         run_operational_model(instance, opt, result_file_path, name, logger)
         write_operational_results(instance, result_file_path, EMISSION_CAP_FLAG, logger)
+
+def set_solver(solver):
+    """Set the solver for the optimization problem and set the parameters (solver dependent). 
+    Solver parameters are currently hardcoded!
+    Solver options:
+    - CPLEX
+    - Xpress
+    - Gurobi
+    - GLPK
+
+    Args:
+        solver (str): The name of the solver to use.
+
+    Returns:
+        SolverFactoryClass: The solver instance.
+    """
+    if solver == "CPLEX":
+        opt = SolverFactory("cplex", Verbose=True)
+        opt.options["lpmethod"] = 4
+        opt.options["solutiontype"] = 2
+        #instance.display('outputs_cplex.txt')
+    if solver == "Xpress":
+        opt = SolverFactory("xpress") #Verbose=True
+        opt.options["defaultAlg"] = 4
+        opt.options["crossover"] = 0
+        opt.options["lpLog"] = 1
+        opt.options["Trace"] = 1
+        #instance.display('outputs_xpress.txt')
+    if solver == "Gurobi":
+        opt = SolverFactory('gurobi', Verbose=True)
+        opt.options["Crossover"]=0
+        opt.options["Method"]=2
+    if solver == "GLPK":
+        opt = SolverFactory("glpk", Verbose=True)
+    return opt
