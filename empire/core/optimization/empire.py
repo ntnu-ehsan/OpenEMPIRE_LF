a606f41d343c0621485e5bbd46a26e4b91b30ce0
WouterKoksNL <wouterkoks135@live.nl>
2025-09-05 13:26:46 +0200

    out of sample

--- empire/core/optimization/empire.py
+++ empire/core/optimization/empire.py
@@ -26,6 +26,7 @@ from pyomo.environ import (
 from .operational_constraints import define_operational_sets, define_operational_constraints, prep_operational_parameters, define_operational_variables, define_operational_parameters, load_operational_parameters
 from .investment_constraints import define_investment_constraints, prep_investment_parameters, define_investment_variables, load_investment_parameters, define_investment_parameters
 from .shared_data import define_shared_sets, load_shared_sets, define_shared_parameters, load_shared_parameters
+from .out_of_sample_functions import set_investments_as_parameters, load_optimized_investments, set_out_of_sample_path
 from .lopf_module import LOPFMethod, load_line_parameters
 from .results import write_results, run_operational_model, write_operational_results, write_pre_solve
 from .solver import set_solver
@@ -249,36 +250,3 @@ def run_empire(instance_name: str,
         write_operational_results(instance, result_file_path, emission_cap_flag, logger)
 
 
-def set_investments_as_parameters(model, data):
-    # Redefine investment vars as input parameters
-    model.genInvCap = Param(model.GeneratorsOfNode, model.PeriodActive, domain=NonNegativeReals)
-    model.transmisionInvCap = Param(model.BidirectionalArc, model.PeriodActive, domain=NonNegativeReals)
-    model.storPWInvCap = Param(model.StoragesOfNode, model.PeriodActive, domain=NonNegativeReals)
-    model.storENInvCap = Param(model.StoragesOfNode, model.PeriodActive, domain=NonNegativeReals)
-    model.genInstalledCap = Param(model.GeneratorsOfNode, model.PeriodActive, domain=NonNegativeReals)
-    model.transmissionInstalledCap = Param(model.BidirectionalArc, model.PeriodActive, domain=NonNegativeReals)
-    model.storPWInstalledCap = Param(model.StoragesOfNode, model.PeriodActive, domain=NonNegativeReals)
-    model.storENInstalledCap = Param(model.StoragesOfNode, model.PeriodActive, domain=NonNegativeReals)
-    return 
-
-
-def load_optimized_investments(model, data, result_file_path):
-    """Optimized investment decisions read from result file from in-sample runs"""
-    data.load(filename=str(result_file_path / 'genInvCap.tab'), param=model.genInvCap, format="table")
-    data.load(filename=str(result_file_path / 'transmisionInvCap.tab'), param=model.transmisionInvCap, format="table")
-    data.load(filename=str(result_file_path / 'storPWInvCap.tab'), param=model.storPWInvCap, format="table")
-    data.load(filename=str(result_file_path / 'storENInvCap.tab'), param=model.storENInvCap, format="table")
-    data.load(filename=str(result_file_path / 'genInstalledCap.tab'), param=model.genInstalledCap, format="table")
-    data.load(filename=str(result_file_path / 'transmissionInstalledCap.tab'), param=model.transmissionInstalledCap, format="table")
-    data.load(filename=str(result_file_path / 'storPWInstalledCap.tab'), param=model.storPWInstalledCap, format="table")
-    data.load(filename=str(result_file_path / 'storENInstalledCap.tab'), param=model.storENInstalledCap, format="table")
-    return 
-
-
-def set_out_of_sample_path(result_file_path, sample_file_path):
-    """Update result_file_path to output for given out_of_sample tree"""
-    sample_tree = get_name_of_last_folder_in_path(sample_file_path)
-    result_file_path = result_file_path / f"OutOfSample/{sample_tree}"
-    if not os.path.exists(result_file_path):
-        os.makedirs(result_file_path)
-    return result_file_path
