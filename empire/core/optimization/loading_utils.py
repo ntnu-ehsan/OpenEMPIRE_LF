6455397f55c9d677f80e2d80fee74b71072d871d
WouterKoksNL <wouterkoks135@live.nl>
2025-09-17 19:26:32 +0200

    important fix to loading set for lists

--- empire/core/optimization/loading_utils.py
+++ empire/core/optimization/loading_utils.py
@@ -111,11 +111,12 @@ def load_parameter(
 
 def load_set(data, model_set, value):
     """Create a temporary .tab file with the specified period and load it into the DataPortal."""
+    breakpoint()
     if isinstance(value, (int, float, str)):
         val = [value]
     else:
         val = value
-    df = pd.DataFrame({ 'Value': val})
+    df = pd.Index(val)
     with tempfile.NamedTemporaryFile(mode="w", suffix=".tab", delete=False) as tmpfile:
         df.to_csv(tmpfile.name, sep="\t", index=False)
         tmpname = tmpfile.name
